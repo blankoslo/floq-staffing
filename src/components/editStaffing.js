66b01357e340e3e6e96db37bf1763ebe0b7fcf0d
John Andre Hestad <jahestad@gmail.com>
2016-07-13 10:24:35 +0200

    Insert Add/Remove buttons in edit-mode
    Clearly missing a lot of styling, but will focus on functionality now.

--- src/components/editStaffing.js
+++ src/components/editStaffing.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { Table, TableBody, TableHeader, TableHeaderColumn,
   TableRow, TableRowColumn } from 'material-ui/Table';
 import TextField from 'material-ui/TextField';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import ContentRemove from 'material-ui/svg-icons/content/remove';
+import IconButton from 'material-ui/IconButton';
 
-const EditStaffing = (props) =>
-   (
+const EditStaffing = props =>
+(
   <div>
     <div style={{ textAlign: 'center' }}>
       <h3>{props.employee.data.name}</h3>
@@ -14,9 +17,9 @@ const EditStaffing = (props) =>
       <Table selectable={false}>
         <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
           <TableRow>
-            <TableHeaderColumn>Uke</TableHeaderColumn>
+            <TableHeaderColumn>Prosjekter</TableHeaderColumn>
             {props.tableData.data.weeks.map((row, index) => (
-              <TableHeaderColumn key={index}>{row.week}</TableHeaderColumn>
+              <TableHeaderColumn key={index}>Uke {row.week}</TableHeaderColumn>
             ))}
           </TableRow>
         </TableHeader>
@@ -26,12 +29,19 @@ const EditStaffing = (props) =>
               <TableRowColumn>{project.name}</TableRowColumn>
               {project.days.map((col, colIndex) => (
                 <TableRowColumn key={colIndex}>
-                  <TextField
-                    floatingLabelText={'Dager:'}
-                    floatingLabelFixed={false}
-                    value={col}
-                    id={`${project.id}-${props.tableData.data.weeks[colIndex]}`}
-                  />
+                  <div>
+                    <IconButton>
+                      <ContentRemove />
+                    </IconButton>
+                    <TextField
+                      value={col}
+                      style={{ width: 20 }}
+                      id={`${project.id}-${props.tableData.data.weeks[colIndex]}`}
+                    />
+                    <IconButton>
+                      <ContentAdd />
+                    </IconButton>
+                  </div>
                 </TableRowColumn>
               ))}
             </TableRow>
@@ -40,7 +50,7 @@ const EditStaffing = (props) =>
       </Table>
     </div>
   </div>
-  );
+);
 
 EditStaffing.propTypes = {
   employee: React.PropTypes.object,
