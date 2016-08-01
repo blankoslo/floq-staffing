49a9d2ae588f30e03f118629c51b3b084c783c7a
John Andre Hestad <jah@blankoslo.no>
2016-08-01 14:32:22 +0200

    Add link to employee's name

--- src/components/view/bodyRow.js
+++ src/components/view/bodyRow.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router';
 import StaffingViewBodyCell from './bodyCell';
 
 const StaffingViewBodyRow = (props) => (
   <tr>
-    <td>{props.employeeName}</td>
+    <td><Link to={`/staffing/edit/${props.employeeId}`}>{props.employeeName}</Link></td>
     {props.weeks.map((w, index) =>
       <StaffingViewBodyCell value={w} key={index} />)}
   </tr>
@@ -11,6 +12,7 @@ const StaffingViewBodyRow = (props) => (
 
 StaffingViewBodyRow.propTypes = {
   employeeName: React.PropTypes.string.isRequired,
+  employeeId: React.PropTypes.number.isRequired,
   weeks: React.PropTypes.object.isRequired,
 };
 
