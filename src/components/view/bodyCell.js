634044df00f94768952b6fc5dd922571df58f7e2
John Andre Hestad <jah@blankoslo.no>
2016-08-08 14:07:20 +0200

    Add view cell styling
    Change to percentage of available days. textColor: 0% staffed = most red. 80% least red. 100% normal black. 100%+ bold black.

--- src/components/view/bodyCell.js
+++ src/components/view/bodyCell.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const StaffingViewBodyCell = (props) => (
-  <td>
-    {props.value}
+  <td style={{ color: props.textColor, fontWeight: props.fontWeight }}>
+    {props.value}%
   </td>
 );
 
 StaffingViewBodyCell.propTypes = {
   value: React.PropTypes.number.isRequired,
+  textColor: React.PropTypes.string.isRequired,
+  fontWeight: React.PropTypes.string.isRequired
 };
 
 export default StaffingViewBodyCell;
--- src/components/view/bodyRow.js
+++ src/components/view/bodyRow.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { Link } from 'react-router';
 import StaffingViewBodyCell from './bodyCell';
 
+const textColor = (value, max) => {
+  if (value >= max) return '#white';
+  return `rgb(${255 - (155 / max * value)},0,0)`;
+};
+
+const fontWeight = (value, max) => (value > max ? 'bold' : 'normal');
+
 const StaffingViewBodyRow = (props) => (
   <tr>
     <td className='mdl-data-table__cell--non-numeric'>
       <Link to={`/staffing/edit/${props.employeeId}`}>{props.employeeName}</Link>
     </td>
     {props.weeks.map((w, index) =>
-      <StaffingViewBodyCell value={w} key={index} />)}
+      <StaffingViewBodyCell
+        value={w * 100 / 5}
+        textColor={textColor(w, 5)}
+        fontWeight={fontWeight(w, 5)}
+        key={index}
+      />)}
   </tr>
 );
 
