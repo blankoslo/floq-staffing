4079c6aa398dc20f9dcd1401bcf0f7be6871fecc
John Andre Hestad <jah@blankoslo.no>
2016-08-09 14:29:48 +0200

    Fix week sum bug
    Using employees datamodel instead of worked_days_reducer.

--- src/selectors/weeksTotalSelector.js
+++ src/selectors/weeksTotalSelector.js
@@ -2,14 +2,14 @@ import { createSelector } from 'reselect';
 import weeksSelector from '../selectors/weeksSelector';
 import employeesSelector from '../selectors/employeesSelector';
 
-const getWeeksTotal = (weeks, workedDaysPerWeek, employees) => {
-  if (employees.loading || workedDaysPerWeek.loading) {
+const getWeeksTotal = (weeks, employees) => {
+  if (employees.loading) {
     return { loading: true, data: null };
   }
   return weeks.map((week, index) =>
     Object.assign({}, week, {
-      sum: workedDaysPerWeek.data.toSetSeq().reduce((result, item) =>
-        result + (item.has(week.week) ? item.get(week.week).days : 0), 0),
+      sum: employees.data.toIndexedSeq().reduce((result, item) =>
+        result + item.weeks.get(index).days, 0),
       staffable: employees.data
         .toIndexedSeq()
         .reduce((result, item) => result + item.weeks.get(index).staffable, 0)
@@ -20,7 +20,6 @@ const getWeeksTotal = (weeks, workedDaysPerWeek, employees) => {
 
 export default createSelector(
   weeksSelector,
-  state => state.worked_days_per_week,
   employeesSelector,
   getWeeksTotal
 );
