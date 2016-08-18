b82ab82d3f1c423930b0e4bc558f1d748bd737af
John Andre Hestad <jah@blankoslo.no>
2016-08-18 10:17:46 +0200

    Redesign edit title and arrows

--- src/components/edit/title.js
+++ src/components/edit/title.js
@@ -3,24 +3,29 @@ import React from 'react';
 const StaffingEditTitle = (props) => (
   <div style={{ textAlign: 'center' }}>
     <h1>{props.employeeName}</h1>
-    <h2>{props.selectedYear}</h2>
-    <span>
-      <button
-        className='mdl-button mdl-js-button mdl-button--fab  mdl-button--icon
-          mdl-js-ripple-effect mdl-button--colored'
-        onClick={props.onBackClick}
-      >
-        <i className='material-icons'>arrow_back</i>
-      </button>
-      <h2 style={{ display: 'inline-block' }}>Uke</h2>
-      <button
-        className='mdl-button mdl-js-button mdl-button--fab  mdl-button--icon
-          mdl-js-ripple-effect mdl-button--colored'
-        onClick={props.onForwardClick}
-      >
-        <i className='material-icons'>arrow_forward</i>
-      </button>
-    </span>
+    <div className='mdl-grid'>
+      <div className='mdl-cell mdl-cell--1-col'>
+        <button
+          className='mdl-button mdl-js-button mdl-button--fab  mdl-button--icon
+            mdl-js-ripple-effect mdl-button--colored'
+          onClick={props.onBackClick}
+        >
+          <i className='material-icons'>arrow_back</i>
+        </button>
+      </div>
+      <div className='mdl-cell mdl-cell--10-col'>
+        <h2>{props.selectedYear}</h2>
+      </div>
+      <div className='mdl-cell mdl-cell--1-col'>
+        <button
+          className='mdl-button mdl-js-button mdl-button--fab  mdl-button--icon
+            mdl-js-ripple-effect mdl-button--colored'
+          onClick={props.onForwardClick}
+        >
+          <i className='material-icons'>arrow_forward</i>
+        </button>
+      </div>
+    </div>
   </div>
 );
 
