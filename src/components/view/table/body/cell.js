d253521b8352289cc98ac3eab26c982179a0f7c8
John Andre Hestad <jah@blankoslo.no>
2016-08-22 19:19:00 +0200

    Fix small bug to always show <td>

--- src/components/view/table/body/cell.js
+++ src/components/view/table/body/cell.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const StaffingViewBodyCell = (props) => {
-  if (props.staffedDays < 1 && props.staffableDays < 1) return null;
-  return (<td
+const StaffingViewBodyCell = (props) =>
+  (<td
     colSpan={7}
     className={
         (props.staffedDays > props.staffableDays ? 'bold' : '')
       + (props.staffedDays < props.staffableDays ? ' medium-color' : '')
     }
   >
-    {`${Math.round(props.staffedDays / props.staffableDays * 100)}%`}
+    {(props.staffedDays < 1 && props.staffableDays < 1) ? '' :
+      `${Math.round(props.staffedDays / props.staffableDays * 100)}%`
+    }
   </td>);
-};
 
 StaffingViewBodyCell.propTypes = {
   staffedDays: React.PropTypes.number.isRequired,
