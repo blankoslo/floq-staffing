e16f9703bfa5ac6dc8339d110f2f1d47e2641faa
Yu He <yu.he@blankoslo.no>
2016-09-05 10:12:36 +0200

    Add chart test to table footer

--- src/components/view/table/footer/index.js
+++ src/components/view/table/footer/index.js
@@ -1,33 +1,46 @@
 import React from 'react';
-import BillablePercentageCell from './billablePercentageCell.js';
-import BillableFractionCell from './billableFractionCell.js';
-import BillableHoursCell from './billableHoursCell.js';
+import BillablePercentageCell from './billablePercentageCell';
+import BillableFractionCell from './billableFractionCell';
+import BillableHoursCell from './billableHoursCell';
+import BillablePercentageChart from '../../chart';
 
-const TableFooter = (props) => (
-  <tfoot className='summary-info'>
-    <tr>
-      <td className='mdl-data-table__cell--non-numeric first-col'>
-        Faktureringsgrad
-      </td>
-      {props.data.map((w, index) =>
-        <BillablePercentageCell staffed={w.staffed} staffable={w.staffable} key={index} />)}
-    </tr>
-    <tr>
-      <td className='mdl-data-table__cell--non-numeric first-col'>
-        Fakturerbare dager
-      </td>
-      {props.data.map((w, index) =>
-        <BillableFractionCell staffed={w.staffed} staffable={w.staffable} key={index} />)}
-    </tr>
-    <tr>
-      <td className='mdl-data-table__cell--non-numeric first-col'>
-        Fakturerbare timer
-      </td>
-      {props.data.map((w, index) =>
-        <BillableHoursCell staffed={w.staffed} key={index} />)}
-    </tr>
-  </tfoot>
-);
+const TableFooter = (props) => {
+  let width;
+  return (
+    <tfoot
+      id='summary-info'
+      className='summary-info'
+      ref={(c) => (width = c.style.offsetWidth)}
+    >
+      <tr>
+        <td colSpan={(props.data.size * 7) + 1}>
+          <BillablePercentageChart width={width} />
+        </td>
+      </tr>
+      <tr>
+        <td className='mdl-data-table__cell--non-numeric first-col'>
+          Faktureringsgrad
+        </td>
+        {props.data.map((w, index) =>
+          <BillablePercentageCell staffed={w.staffed} staffable={w.staffable} key={index} />)}
+      </tr>
+      <tr>
+        <td className='mdl-data-table__cell--non-numeric first-col'>
+          Fakturerbare dager
+        </td>
+        {props.data.map((w, index) =>
+          <BillableFractionCell staffed={w.staffed} staffable={w.staffable} key={index} />)}
+      </tr>
+      <tr>
+        <td className='mdl-data-table__cell--non-numeric first-col'>
+          Fakturerbare timer
+        </td>
+        {props.data.map((w, index) =>
+          <BillableHoursCell staffed={w.staffed} key={index} />)}
+      </tr>
+    </tfoot>
+  );
+};
 
 TableFooter.propTypes = {
   data: React.PropTypes.object,
