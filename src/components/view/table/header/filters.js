e63d28ba37ca75e9760fce512982c2b26367ee5d
John Andre Hestad <jah@blankoslo.no>
2016-10-19 10:42:06 +0200

    Remove inactive/unusable filters

--- src/components/view/table/header/filters.js
+++ src/components/view/table/header/filters.js
@@ -2,40 +2,7 @@
 import React from 'react';
 
 const StaffingViewTableHeaderFilters = (props : Object) => (
-  <th rowSpan={props.rowspan} className={'first-col'}>
-    <ul className='mdl-list'>
-      <li className='mdl-list__item'>
-        <label
-          className='mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect'
-          htmlFor='filter-checkbox-1'
-        >
-          <input
-            type='checkbox'
-            style={{ width: '20px' }}
-            id='filer-checkbox-1'
-            disabled
-            className='mdl-checkbox__input'
-          />
-          <span className='mdl-checkbox__label'>Kun med ledig tid</span>
-        </label>
-      </li>
-      <li className='mdl-list__item'>
-        <label
-          className='mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect'
-          htmlFor='filter-checkbox-2'
-        >
-          <input
-            type='checkbox'
-            style={{ width: '20px' }}
-            id='filer-checkbox-2'
-            disabled
-            className='mdl-checkbox__input'
-          />
-          <span className='mdl-checkbox__label'>Annet filter</span>
-        </label>
-      </li>
-    </ul>
-  </th>
+  <th rowSpan={props.rowspan} className={'first-col'} />
 );
 
 StaffingViewTableHeaderFilters.propTypes = {
